7d07f7da49fd9ebe1f4136eec88ae5689dd4359b
Mirza Ćupina <mirza@ito.ba>
2019-10-16 15:12:11 +0200

    Maintenance middleware implementation

---
+++ Corvo.AspNetCore.Mvc/Middleware/Maintenance/MaintenanceConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Corvo.AspNetCore.Mvc.Middleware.Maintenance
+{
+    /// <summary>
+    /// Enables configuration for <see cref="MaintenanceMiddleware"/>
+    /// </summary>
+    /// <remarks>
+    /// Add list for allowed IP address ?
+    /// </remarks>
+    public class MaintenanceConfiguration
+    {
+        private Func<bool> _isEnabled;
+        private byte[] _response;
+
+        public MaintenanceConfiguration(Func<bool> isEnabled, byte[] response)
+        {
+            _isEnabled = isEnabled;
+            _response = response;
+        }
+
+        public bool IsEnabled => _isEnabled();
+        public byte[] Response => _response;
+
+        /// <summary>
+        /// Retry-After header value
+        /// </summary>
+        public int RetryAfterSecondsOffset { get; set; } = 3600;
+
+        public string ContentType { get; set; } = "text/html";
+    }
+}
