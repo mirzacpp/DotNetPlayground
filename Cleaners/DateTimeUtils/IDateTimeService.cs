4dd0d055f093136b43f744264edd600b8b225c8f
mirzacp <41125497+mirzacp@users.noreply.github.com>
2019-12-15 17:57:30 +0100

    Added IDateTimeService interface

---
+++ Cleaners/DateTimeUtils/IDateTimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cleaners.DateTimeUtils
+{
+    /// <summary>
+    /// Date and time abstractions
+    /// </summary>
+    public interface IDateTimeService
+    {
+        DateTime UtcNow { get; }
+    }
+}
