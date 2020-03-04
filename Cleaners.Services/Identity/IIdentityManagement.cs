dfc4971649a5b1f5f513fafe440bcd4acc6644d5
Mirza Ćupina <mirza@ito.ba>
2020-03-04 19:02:01 +0100

    Some updates

--- Cleaners.Services/Identity/IIdentityManagement.cs
+++ Cleaners.Services/Identity/IIdentityManagement.cs
@@ -1,9 +1,16 @@
-﻿namespace Cleaners.Services.Identity
+﻿using Cleaners.Core.Domain;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Cleaners.Services.Identity
 {
     /// <summary>
     /// Wraps all abstractions for AspNetCore Identity
     /// </summary>
     public interface IIdentityManagement
     {
+        IdentityOptions Options { get; }
+
+        Task<User> FindByNameAsync(string userName);
     }
 }
