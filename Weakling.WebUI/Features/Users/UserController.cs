892eb438dfdda32a0a7d3fd01edbe869bd4e4771
Mirza Ćupina <mirza@ito.ba>
2021-11-01 23:09:56 +0100

    Aaalo

---
+++ Weakling.WebUI/Features/Users/UserController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Weakling.WebUI.Features.Users
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
