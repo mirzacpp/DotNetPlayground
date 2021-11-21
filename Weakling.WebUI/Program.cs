cab146a282c85ac0b2423ff22ffb146817c7dd6a
Mirza Ćupina <mirza@ito.ba>
2021-11-21 23:34:02 +0100

    Ajmooo CE

--- Weakling.WebUI/Program.cs
+++ Weakling.WebUI/Program.cs
@@ -1,29 +1,70 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
+using Serilog.Extensions.Hosting;
 
-// Add services to the container.
-builder.Services
-    .AddControllersWithViews()
-    .AddFeatureFolders();
+namespace Weakling.WebUI;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+/// <summary>
+/// TODO: Add Serilog
+/// </summary>
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static async Task<int> Main(string[] args)
+    {
+        Log.Logger = CreateBootstrapLogger();
+
+        IHost? host = null;
+
+        try
+        {
+            Log.Information("Initialising.");
+
+            host = CreateHostBuilder(args).Build();
+
+            host.LogApplicationStarted();
+            await host.RunAsync();
+            host.LogApplicationStopped();
+
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            host!.LogApplicationTerminatedUnexpectedly(ex);
+
+            return 1;
+        }
+        finally
+        {
+            // Flush logger
+            Log.CloseAndFlush();
+        }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .UseDefaultServiceProvider((context, options) =>
+               {
+                   var isDevelopment = context.HostingEnvironment.IsDevelopment();
+
+                   options.ValidateScopes = isDevelopment;
+                   options.ValidateOnBuild = isDevelopment;
+               })
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.UseStartup<Startup>();
+               });
+
+    #region Logging utils
+
+    /// <summary>
+    /// Creates a logger used during application initialisation.
+    /// <see href="https://nblumhardt.com/2020/10/bootstrap-logger/"/>.
+    /// </summary>
+    /// <returns>A logger that can load a new configuration.</returns>
+    private static ReloadableLogger CreateBootstrapLogger() =>
+        new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .CreateBootstrapLogger();
+
+    #endregion Logging utils
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
