8fdc16115c29f0fc7a45910f37b4a2ea6b4fcf02
Mirza Ćupina <mirza@ito.ba>
2022-03-01 23:46:44 +0100

    Console app updates

--- Studens.Net6.ConsoleUI/Program.cs
+++ Studens.Net6.ConsoleUI/Program.cs
@@ -29,74 +29,9 @@
 //Console.WriteLine($"{RomanToInt("III")} == 3");
 //Console.WriteLine($"{RomanToInt("LVIII")} == 58");
 //Console.WriteLine($"{RomanToInt("MCMXCIV")} == 1994");
-var testCases = new[]
-{
-    "(){",
-    "()[]{}",
-    "(]",
-    "{[(]]}",
-    "{[(n]]}",
-    "{[()]}",
-    "[[[]",
-    "()))"
-};
 
-bool IsValid(string s)
-{
-    var length = s.Length;
+using Studens.Net6.ConsoleUI;
 
-    // Some base validation
-    if (length < 2) return false;
-    if (length % 2 != 0) return false;
-
-    var brackets = new Dictionary<char, char>
-    {
-        { '(', ')' },
-        { '[', ']' },
-        { '{', '}' },
-    };
-
-    // validate last and first chars
-    if (!brackets.ContainsKey(s[0]) || !brackets.ContainsValue(s[length - 1]))
-    {
-        return false;
-    }
-
-    var stack = new Stack<char>();
-    stack.Push(s[0]);
-
-    for (int i = 1; i < s.Length; i++)
-    {
-        // Is it a open bracket ?
-        if (brackets.ContainsKey(s[i]))
-        {
-            stack.Push(s[i]);
-        }
-        else if (stack.Count > 0)
-        {
-            var lastOpeningBracket = stack.Pop();
-
-            if (brackets[lastOpeningBracket] != s[i])
-            {
-                return false;
-            }
-        }
-    }
-
-    return stack.Count == 0;
-}
-
-foreach (var test in testCases)
-{
-    var result = IsValid(test);
-    if (result)
-    {
-        Console.WriteLine($"Test result for '{test}' is valid.");
-    }
-    else
-    {
-        Console.WriteLine($"Test result for '{test}' is not valid because");
-    }
-}
+LeetCode.RunPalindromeProblem();
 
 Console.ReadLine();
