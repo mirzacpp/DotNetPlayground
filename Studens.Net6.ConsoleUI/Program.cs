e1d327d01a6a8bc1ebc1abadd057cbe106087c66
Mirza Ćupina <mirza@ito.ba>
2022-04-04 08:58:21 +0200

    Sources updates

--- Studens.Net6.ConsoleUI/Program.cs
+++ Studens.Net6.ConsoleUI/Program.cs
@@ -1,35 +1,61 @@
 ﻿//LeetCode.RunLengthOfLastWordProblem();
 
-var cts = new CancellationTokenSource();
-
-ThreadPool.QueueUserWorkItem(_ => Count(cts.Token, 1000));
-Console.WriteLine("Press enter to cancel the operation.");
-Console.ReadKey();
-cts.Cancel();
-
-//LeetCode.RunShuffleStringProblem();
-
-Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-var task = new Task(() => Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is printing message."));
-task.Start();
-
-Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-
-Console.ReadLine();
-
-void Print()
+await foreach (var item in YieldAsync())
 {
-    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is printing message.");
+    Console.WriteLine($"Captured {item}");
 }
 
-async void AwaitVoid()
+IEnumerable<int> YieldSync()
 {
-    await Task.CompletedTask;
-    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is awaiting void.");
+    for (int i = 1; i <= 3; i++)
+    {
+        Console.WriteLine($"Return {i}");
+        yield return i;
+        Console.WriteLine("Move on.");
+    }
 }
 
-async Task AwaitTask()
+async IAsyncEnumerable<int> YieldAsync()
 {
-    await Task.CompletedTask;
-    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is awaiting Task.");
+    for (int i = 1; i <= 3; i++)
+    {
+        Console.WriteLine($"Return {i}");
+        yield return i;
+        Console.WriteLine($"Wait for {1_000 * i}ms");
+        await Task.Delay(1_000 * i);        
+    }
 }
+
+//var cts = new CancellationTokenSource();
+
+//ThreadPool.QueueUserWorkItem(_ => Count(cts.Token, 1000));
+//Console.WriteLine("Press enter to cancel the operation.");
+//Console.ReadKey();
+//cts.Cancel();
+
+////LeetCode.RunShuffleStringProblem();
+
+//Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+//var task = new Task(() => Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is printing message."));
+//task.Start();
+
+//Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+//Console.ReadLine();
+
+//void Print()
+//{
+//    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is printing message.");
+//}
+
+//async void AwaitVoid()
+//{
+//    await Task.CompletedTask;
+//    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is awaiting void.");
+//}
+
+//async Task AwaitTask()
+//{
+//    await Task.CompletedTask;
+//    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is awaiting Task.");
+//}
