fa56e078717f768503e4dcf3f4b8383fb6c7e1c5
Mirza Ćupina <mirza@ito.ba>
2022-07-06 15:23:45 +0200

    Sources

--- Studens.Data/Extensions/JsonHelper.cs
+++ Studens.Data/Extensions/JsonHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Diagnostics;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Studens.Data.Extensions
--- Studens.Net6.ConsoleUI/Program.cs
+++ Studens.Net6.ConsoleUI/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.ReadKey();
+﻿var cts = new CancellationTokenSource();
+
+//var tasks = new[] { 2000, 1000, 3000, 4000, 5000 }
+//.Select(t => DoSomeWorkAsync(t));
+
+Console.WriteLine("=====> Application started");
+Console.WriteLine("=====> Press ENTER key to cancel ...");
+
+Task cancelTask = Task.Run(() =>
+{
+	while (Console.ReadKey().Key != ConsoleKey.Enter)
+	{
+		Console.WriteLine("=====> Press ENTER key to cancel ...");
+	}
+
+	Console.WriteLine("\n\n\n=====> ENTER pressed, cancelling ...");
+	cts.Cancel();	
+});
+
+await Task.WhenAny(new[] { cancelTask, DoSomeWorkAsync() });
+
+Console.WriteLine("=====> Application stopped");
+
+async Task DoSomeWorkAsync()
+{
+	foreach (var delay in new[] { 2000, 1000, 3000, 4000, 5000, 10_000 })
+	{
+		Console.WriteLine($"I will delay for {delay}");
+		await Task.Delay(delay, cts!.Token);
+		Console.WriteLine($"I have completed after delay of {delay}ms");
+	}
+}
